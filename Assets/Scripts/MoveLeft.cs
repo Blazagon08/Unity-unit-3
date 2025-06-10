97b567b9465955b3492599987ec411cf1bf4ac5e
Ortiz_Josiah <ortij114@hsd.k12.or.us>
2025-06-10 16:25:16 -0700

    Finishing 3.1

---
+++ Assets/Scripts/MoveLeft.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class MoveLeft : MonoBehaviour
+{
+    private float speed = 30;
+
+    // Start is called once before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.left * Time.deltaTime * speed);
+    }
+}
